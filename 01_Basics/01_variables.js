fb8ef4fb6fd77dac07e1c478d120c338c66fba5f
Rohit Barge <139971224+rbarge-paktolus@users.noreply.github.com>
2023-12-26 10:35:33 +0000

    variable and constant

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14443
+let accountEmail = "Rohit@google.com"
+var accountPassword = "553532"
+accountCity = "Mumbai"
+let accountState
+
+//accountId = 2
+
+accountEmail="rbarge@paktolus.com"
+accountPassword = "23232"
+accountCity = "Navi Mumbai"
+
+console.log(accountId);
+
+/*
+Prefer note to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
